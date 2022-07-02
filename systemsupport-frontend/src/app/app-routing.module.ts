a7a5a4c7df0538c0bf72729fba4026b54d64030a
Milica Urukalo <urukalo.ra38.2018@uns.ac.rs>
2022-07-03 00:59:09 +0200

    Home front

--- systemsupport-frontend/src/app/app-routing.module.ts
+++ systemsupport-frontend/src/app/app-routing.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BayesComponent } from './bayes/bayes.component';
+import { HomeComponent } from './core/home/home.component';
 import { FuzzyComponent } from './fuzzy/fuzzy.component';
 import { SuggestionComponent } from './suggestion/suggestion.component';
 
 const routes: Routes = [
-  {path:'suggestion',component:SuggestionComponent},
-  {path:'fuzzy',component:FuzzyComponent},
-  {path:'bayes',component:BayesComponent}
+  {path:'suggestion',component: SuggestionComponent},
+  {path:'fuzzy',component: FuzzyComponent},
+  {path:'bayes',component: BayesComponent},
+  { path: 'home', component: HomeComponent},
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
 
 @NgModule({
--- systemsupport-frontend/src/app/app.module.ts
+++ systemsupport-frontend/src/app/app.module.ts
@@ -3,18 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavBarComponent } from './core/nav-bar/nav-bar.component';
 import { SuggestionComponent } from './suggestion/suggestion.component';
 import { FuzzyComponent } from './fuzzy/fuzzy.component';
 import { BayesComponent } from './bayes/bayes.component';
+import { NavBarComponent } from './core/nav-bar/nav-bar.component';
+import { HomeComponent } from './core/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent,
     SuggestionComponent,
     FuzzyComponent,
-    BayesComponent
+    BayesComponent,
+    NavBarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
