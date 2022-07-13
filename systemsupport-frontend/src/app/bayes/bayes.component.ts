4e0ceb6ee8b8a77a82415d4ecd7a8b152fc3b4da
ra149 <fresh.trifunovic@gmail.com>
2022-07-13 21:48:23 +0200

    bayes front done

--- systemsupport-frontend/src/app/bayes/bayes.component.ts
+++ systemsupport-frontend/src/app/bayes/bayes.component.ts
@@ -75,6 +75,86 @@ export class BayesComponent implements OnInit {
             }
             break;
           }
+          case "pc_crash_before_loading_os":{
+            if(response[i].failure == "OS DAMAGE" || response[i].failure == "HDD DAMAGE"  || response[i].failure == "RAM DAMAGE"){
+              filtrirano.push(response[i]);
+            }
+            break;
+          }
+          case "sound_isnt_working":{
+            if(response[i].failure == "SPEAKERS DAMAGE" || response[i].failure == "BLUETOOTH CONNECTION DAMAGE"  || response[i].failure == "SOUND CARD DAMAGE"  || response[i].failure == "PORT DAMAGE"){
+              filtrirano.push(response[i]);
+            }
+            break;
+          }
+          case "wifi_disconnecting":{
+            if(response[i].failure == "NETWORK CARD DAMAGE"){
+              filtrirano.push(response[i]);
+            }
+            break;
+          }
+          case "slow_pc":{
+            if(response[i].failure == "ANTIVIRUS DAMAGE" || response[i].failure == "STARTUP PROGRAMS DAMAGE" || response[i].failure == "HDD DAMAGE" || response[i].failure == "RAM DAMAGE"){
+              filtrirano.push(response[i]);
+            }
+            break;
+          }
+          case "slow_internet":{
+            if(response[i].failure == "POWER SUPPLY DAMAGE" || response[i].failure == "ROUTER DAMAGE"  || response[i].failure == "PROVIDER DAMAGE"  || response[i].failure == "CABLE DAMAGE"){
+              filtrirano.push(response[i]);
+            }
+            break;
+          }
+
+          case "frozen_screen":{
+            if(response[i].failure == "HDD DAMAGE" || response[i].failure == "OS DAMAGE" || response[i].failure == "RAM DAMAGE" || response[i].failure == "DRIVER DAMAGE"){
+              filtrirano.push(response[i]);
+            }
+            break;
+          }
+
+          case "computer_beeps":{
+            if(response[i].failure == "RAM DAMAGE"){
+              filtrirano.push(response[i]);
+            }
+            break;
+          }
+
+          case "blue_screen_of_death":{
+            if(response[i].failure == "COOLING SYSTEM DAMAGE" || response[i].failure == "RAM DAMAGE" || response[i].failure == "MOTHERBOARD DAMAGE" || response[i].failure == "HDD DAMAGE"){
+              filtrirano.push(response[i]);
+            }
+            break;
+          }
+
+          case "mouse_malfunction":{
+            if(response[i].failure == "CABLE DAMAGE" || response[i].failure == "DRIVER DAMAGE" || response[i].failure == "PORT DAMAGE" || response[i].failure == "PHYSICAL DAMAGE"){
+              filtrirano.push(response[i]);
+            }
+            break;
+          }
+
+          case "usb_port_malfunction":{
+            if(response[i].failure == "PHYSICAL DAMAGE" || response[i].failure == "PINS DAMAGE" || response[i].failure == "DRIVER DAMAGE") {
+              filtrirano.push(response[i]);
+            }
+            break;
+          }
+          
+          case "usb_wont_load":{
+            if(response[i].failure == "ANTIVIRUS DAMAGE" || response[i].failure == "HDD DAMAGE" || response[i].failure == "DRIVER DAMAGE"){
+              filtrirano.push(response[i]);
+            }
+            break;
+          }
+
+          
+          case "keyboard_malfunction":{
+            if(response[i].failure == "PHYSICAL DAMAGE" || response[i].failure == "DRIVER DAMAGE" || response[i].failure == "BLUETOOTH CONNECTION DAMAGE" || response[i].failure == "PORT DAMAGE"){
+              filtrirano.push(response[i]);
+            }
+            break;
+          }
 
         }
 
